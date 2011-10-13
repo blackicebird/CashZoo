af66143b49c10a87055de48f370866301afac2ff
Leon Lee <blackicebird@yahoo.com.cn>
2011-10-13 10:37:58 +0800

    desing transaction

--- cashCat/app/models/register.js
+++ cashCat/app/models/register.js
@@ -6,21 +6,46 @@
  * Email: mail.lgq@gmail.com
  */
 
-Ext.regModel('Transaction', {
+Ext.regModel('Entry', {
     fields: [
-        {name: 'tranDate', type: 'date'},
-        {name: 'seq', type: 'int'},
+        {name: 'transaction', type: 'string'},
+        {name: 'action', type: 'string'},
         {name: 'description', type: 'string'},
         {name: 'from', type: 'string'},
         {name: 'to', type: 'string'},
-        {name: 'amount', type: 'float'}
+        {name: 'amount', type: 'float'},
+        {name: 'balance', type: 'float'}
+        
     ],
     validations: [
-        {type: 'presence', field: 'tranDate'},
         {type: 'presence', field: 'from'},
         {type: 'presence', field: 'to'},
-        {type: 'presence', field: 'amount'},
-        {type: 'presence', field: 'ref'}
+        {type: 'presence', field: 'amount'}
+    ],
+    proxy: {
+        type: "websqlproxy",
+        dbName: cashCat.dbName,
+        dbTable: 'entry',
+        dbVersion: '1.0',
+        writer: {
+            type: 'json',
+            writeAllFields: false
+        },
+        initialData: [
+        ]
+    }
+});
+
+Ext.regModel('Transaction', {
+    fields: [
+        {name: 'postedDate', type: 'date'},
+        {name: 'seq', type: 'int'},
+        {name: 'description', type: 'string'},
+        {name: 'total', type: 'float'}
+    ],
+    validations: [
+        {type: 'presence', field: 'postedDate'},
+        {type: 'presence', field: 'total'}
     ],
     proxy: {
         type: "websqlproxy",
@@ -35,5 +60,3 @@ Ext.regModel('Transaction', {
         ]
     }
 });
-
-Ext.regModel('TransactionGroup')
